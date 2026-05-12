1d517595129a5bef826f9de342a24fef019d97f8
geekyjunk <naman.geek@gmail.com>
2026-05-12 19:58:34 +0530

    feat: add commander + basic command

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-console.log("Stitchr ran successfully...");
+const { Command } = require("commander");
+const pkg = require("./package.json");
+
+const program = new Command();
+
+program
+  .name("stitchr")
+  .description("Stitchr CLI")
+  .version(pkg.version);
+
+program
+  .command("hello")
+  .description("Print a friendly greeting")
+  .argument("[name]", "name to greet", "world")
+  .option("-u, --uppercase", "shout the greeting")
+  .action((name, options) => {
+    let message = `Hello, ${name}!`;
+    if (options.uppercase) {
+      message = message.toUpperCase();
+    }
+    console.log(message);
+  });
+
+program.parse(process.argv);
